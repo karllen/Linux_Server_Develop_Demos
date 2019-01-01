99713e21ed8963ced960ef03f0ff287e24fd4430
karllen <1160113606@qq.com>
2019-01-01 20:13:04 +0800

    add library I used

--- Effective Modern C++/Smart Pointer/Item21.h
+++ Effective Modern C++/Smart Pointer/Item21.h
@@ -1,1 +1,9 @@
 //Item 21:Prefer std::make_unique and std::make_shared to direct use of new
+
+#include<memory>
+
+template<typename T, typename... Ts>
+std::unique_ptr<T> make_unique(Ts&&...params)
+{
+	return std::unique_ptr<T>(new T(std::forward<Ts>(params)...);
+}
